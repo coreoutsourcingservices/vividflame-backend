d92e82321047d48222bd3f0860bda4e9477cd61c
prahlad800 <prahlad8009872468@gmail.com>
2026-07-06 16:12:05 +0530

    fix

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -30,6 +30,10 @@ router.post("/signup-verifyOTP", verifyOTP)
 router.post("/login-verifyOTP", verifyOTP_login)
 
 // address
+// Price Routes
+router.post("/price/create", createPrice);
+router.get("/product/:user", getProductsByUser);
+
 router.post("/add", addAddress);
 router.get("/:user", getAddresses);
 router.put("/:user/:addressId", updateAddress);
